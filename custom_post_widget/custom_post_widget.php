141a4c7c6cf16dea89598c5391d0490dc1ceac8f
joosemi1993 <joosemi.1993@gmail.com>
2019-05-10 15:58:24 +0200

    Defining the Widget

---
+++ custom_post_widget/custom_post_widget.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Custom Post Widget
+Plugin URI:  
+Description: Widget where you can set your 3 favourite posts for your sidebar
+Version:     1.0.0
+Author:      Josemi Calvo
+Author URI:  https://josemicalvo.com/
+Text Domain: cpw
+Domain Path: 
+License:     GPL2
+ 
+Custom Post Widget is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Custom Post Widget is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Custom Post Widget.
+
+*/
